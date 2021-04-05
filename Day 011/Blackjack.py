187de95ce86ee9e642983c21088446b59675644a
Omar AbdElkareem <omarkimo80@gmail.com>
2021-04-06 01:21:27 +0200

    Day 11

---
+++ Day 011/Blackjack.py
@@ -0,0 +1,39 @@
+import random
+import os 
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower() == 'y':
+    os.system('cls' if os.name == 'nt' else 'clear')
+    # ascii art
+    print('''
+    ''')
+    
+    user_cards = random.choices(cards, k = 2)
+    com_cards = random.choices(cards, k = 2)
+    print(f"Your cards: {user_cards}, current score: {sum(user_cards)}")
+    print(f"Computer's first card: {com_cards[0]}")
+    while input("Type 'y' to get another card, or 'n' to pass: ").lower() == 'y': 
+        user_cards.append(random.choice(cards))
+        print(f"Your cards: {user_cards}, current score: {sum(user_cards)}")
+        print(f"Computer's first card: {com_cards[0]}")
+        if sum(user_cards) > 21:
+            break
+    while sum(com_cards) < 17:
+        com_cards.append(random.choice(cards))
+    
+    s1 = sum(user_cards)
+    s2 = sum(com_cards)
+    print(f"Your final hand: {user_cards}, final score: {s1}")
+    print(f"Computer's final hand: {com_cards}, final score: {s2}")
+    
+    if s1 > 21:
+        print("You went over. You lose. ;_;")
+    elif s1 == s2:
+        print("It's a draw. -_-")
+    elif s1 < s2:
+        print("You lose. ;_;")
+    else:
+        print("You win. ^_^")
+    
+print("Goodbye.")
